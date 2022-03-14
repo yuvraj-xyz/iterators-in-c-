9817fca95f9eb3c5ecf5583e4e3f2bfcf402a8e8
code_yuvi <78093228+yuvicpp@users.noreply.github.com>
2022-03-14 13:28:48 -0600

    Create iterators.cpp

---
+++ iterators.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[6] = {1,2,3,4,5,6};
+    for(auto it: arr.begin(); it<arr.end(); it++)
+    {
+        cout << *it << " ";
+    }
+
+    // int arr[6] = {1,2,3,4,5,6};
+    // for(auto it: arr.rbegin(); it<arr.rend(); it++)
+    // {
+    //     cout << *it << " ";
+    // }
+
+
+    // int arr[6] = {1,2,3,4,5,6};
+    // for(auto it: arr)
+    // {
+    //     cout << it << " ";
+    // }
+}
+
